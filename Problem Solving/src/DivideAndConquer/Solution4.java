d552a6e6a943f6604d8abde1436911b86dd6939c
gvams <gvamsi244@gmail.com>
2020-08-15 01:57:26 -0400

    Updated return value

---
+++ Problem Solving/src/DivideAndConquer/Solution4.java
@@ -0,0 +1,28 @@
+package DivideAndConquer;
+
+/**
+ * @author Vamsi Krishna Govada on 8/14/2020.
+ * @project Raudra
+ */
+
+// Write a custom c-function to implement pow() function
+
+public class Solution4 {
+
+    private static int power(int x, int y) {
+        if(y==1)
+            return x;
+        if(y%2==0)
+            return power(x,y/2)*power(x,y/2);
+        else
+            return x*power(x,y/2)*power(x,y/2);
+    }
+
+    public static void main(String[] args)
+    {
+        int x = 2;
+        int y = 3;
+
+        System.out.println(power(x, y));
+    }
+}
