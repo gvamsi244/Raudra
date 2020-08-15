c63c84d3bac4d578e42e641cc050e146e8ddfbb5
gvams <gvamsi244@gmail.com>
2020-08-15 02:00:00 -0400

    Added Solution

--- Problem Solving/src/DivideAndConquer/Solution4.java
+++ Problem Solving/src/DivideAndConquer/Solution4.java
@@ -18,8 +18,7 @@ public class Solution4 {
             return x*power(x,y/2)*power(x,y/2);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int x = 2;
         int y = 3;
 
